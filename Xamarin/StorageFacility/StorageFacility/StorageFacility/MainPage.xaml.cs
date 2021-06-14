b9e6185d7327d0fac551bc577892fd0a4de0da94
darkmtbg2 <darkmtbg2@gmail.com>
2021-06-14 09:52:47 +0200

    Stub for Xamarin app and create rack

--- Xamarin/StorageFacility/StorageFacility/StorageFacility/MainPage.xaml.cs
+++ Xamarin/StorageFacility/StorageFacility/StorageFacility/MainPage.xaml.cs
@@ -10,9 +10,25 @@ namespace StorageFacility
 {
     public partial class MainPage : ContentPage
     {
+        APIService apiService = new APIService();
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void Create_New_Rack(object sender, EventArgs e)
+        {
+            string result = await DisplayPromptAsync("New Rack", "Enter Rack Name:");
+            bool resultbool = await apiService.CreateRack(result);
+            if (resultbool)
+            {
+                await DisplayAlert("New Rack", "New rack have been created", "OK");
+            }
+            else
+            {
+                await DisplayAlert("New Rack", "New rack have not been created", "OK");
+            }
         }
     }
 }
