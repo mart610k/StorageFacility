5c591c461ab2a0abe13a39ef75ab939f9bd19251
darkmtbg2 <darkmtbg2@gmail.com>
2021-06-15 13:27:28 +0200

    Moved the code into service namespace and added the SQL for the Database

---
+++ API/StorageFacility/StorageFacility/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StorageFacility.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace StorageFacility.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        IProductLogic productLogic = new ProductLogic();
+
+
+        [HttpPost]
+        public IActionResult RegisterProduct(string barcode, string productName)
+        {
+            try
+            {
+                productLogic.RegisterProduct("", barcode, productName);
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(400,"Test");
+            }
+        }
+
+
+    }
+}
--- API/StorageFacility/StorageFacility/Logic/ProductLogic.cs
+++ API/StorageFacility/StorageFacility/Logic/ProductLogic.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using StorageFacility.Classes;
 using StorageFacility.Exceptions;
+using StorageFacility.Service;
 
 namespace StorageFacility.Logic
 {
