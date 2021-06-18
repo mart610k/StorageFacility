898855b04cc5aceacaabb1a231f1c0a11690b203
Leo Jensen <lion0005@zbc.dk>
2021-06-18 10:34:06 +0200

    Add Product To Shelf is now done, Tested on Emulator, and comments on the Code

--- Xamarin/StorageFacility/StorageFacility/StorageFacility/Service/IShelfService.cs
+++ Xamarin/StorageFacility/StorageFacility/StorageFacility/Service/IShelfService.cs
@@ -1,9 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using StorageFacility.Class;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace StorageFacility.Service
 {
     interface IShelfService
     {
         Task<bool> CreateShelf(string name, string rackName);
+        Task<bool> AddProductToShelf(string rackName, string shelfName, string barcode);
+        Task<List<Shelf>> GetShelves();
     }
 }
