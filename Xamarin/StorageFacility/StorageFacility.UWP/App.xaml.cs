916cf31805d5bca6cb4a5014c47c2de785e52d5a
darkmtbg2 <darkmtbg2@gmail.com>
2021-06-21 14:19:28 +0200

    removed some code not used

--- Xamarin/StorageFacility/StorageFacility.UWP/App.xaml.cs
+++ Xamarin/StorageFacility/StorageFacility.UWP/App.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
@@ -30,7 +29,6 @@ namespace StorageFacility.UWP
         public App()
         {
             this.InitializeComponent();
-            
             this.Suspending += OnSuspending;
         }
 
